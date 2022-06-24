fa0394326fff33d60d6b8386b3f236d637303a38
伍东京 <15211420607@163.com>
2022-06-24 23:49:08 +0800

    second

--- scripts/pre-build.js
+++ scripts/pre-build.js
@@ -1,20 +1,19 @@
-const path = require('path')
-const { preBuild } = require('../dist/index')
+const path = require('path');
+const { preBuild } = require('../dist/index');
 
 preBuild({
-    apps: [
-        {
-            label: 'pre-build',
-            name: 'pre-build',
-            projectPath: path.join(__dirname, '../'),
-        },
-    ],
-    envs: [
-        {
-            name: 'prd',
-            identifier: '',
-        },
-    ],
-    prdAppEnv: 'prd',
-    tagEnvs: ['prd'],
-})
+  apps: {
+    label: 'pre-build',
+    name: 'pre-build',
+    projectPath: path.join(__dirname, '../'),
+  },
+
+  envs: [
+    {
+      name: 'prd',
+      identifier: '',
+    },
+  ],
+  prdAppEnv: 'prd',
+  tagEnvs: ['prd'],
+});
