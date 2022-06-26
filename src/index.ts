6ac5d100064f0c8d4ce89795dc9c2909efc53371
伍东京 <15211420607@163.com>
2022-06-26 11:27:56 +0800

    优化

--- src/index.ts
+++ src/index.ts
@@ -1,237 +1,237 @@
-import { notify, handleUrlAsign } from './dingNotify';
-const chalk = require('chalk');
-const logSymbols = require('log-symbols');
-const path = require('path');
-const fs = require('fs');
-const yargs = require('yargs');
-const enquirer = require('enquirer');
+import { notify, handleUrlAsign } from './dingNotify'
+const chalk = require('chalk')
+const logSymbols = require('log-symbols')
+const path = require('path')
+const fs = require('fs')
+const yargs = require('yargs')
+const enquirer = require('enquirer')
 // const figures = require('figures');
-const semver = require('semver');
-const simplegit = require('simple-git');
-const branch = require('./branch');
+const semver = require('semver')
+const simplegit = require('simple-git')
+const branch = require('./branch')
 
 // 计算下一个版本号
 function nextVersion(
-  version,
-  releaseType = 'patch',
-  identifier = '',
+    version,
+    releaseType = 'patch',
+    identifier = ''
 ): Promise<string> {
-  return semver.inc(version, releaseType, identifier);
+    return semver.inc(version, releaseType, identifier)
 }
 
 // 修改 package.json 的版本号
 function changeVersion(
-  version: string,
-  pkgConfig: any,
-  pkgConfigFile: string,
+    version: string,
+    pkgConfig: any,
+    pkgConfigFile: string
 ): Promise<string> {
-  version = String(version).trim();
-  return new Promise((resolve, reject) => {
-    if (version.trim() === pkgConfig.version) {
-      resolve(version);
-      return version;
-    }
-    pkgConfig.version = version;
-    const packageJSON = JSON.stringify(pkgConfig, null, 4) + '\n';
-    fs.writeFile(pkgConfigFile, packageJSON, 'utf8', (err) => {
-      if (err) {
-        return reject(err);
-      }
-      resolve(version);
-    });
-  });
+    version = String(version).trim()
+    return new Promise((resolve, reject) => {
+        if (version.trim() === pkgConfig.version) {
+            resolve(version)
+            return version
+        }
+        pkgConfig.version = version
+        const packageJSON = JSON.stringify(pkgConfig, null, 4) + '\n'
+        fs.writeFile(pkgConfigFile, packageJSON, 'utf8', (err) => {
+            if (err) {
+                return reject(err)
+            }
+            resolve(version)
+        })
+    })
 }
 
 export interface configOptions {
-  /*! 钉钉群机器人 */
-  dingTalk?: { url: string; asign: string };
-  /*! 项目配置 */
-  apps: Apps;
-  /*! 项目环境配置 */
-  envs: Envs[];
-  /*! 项目生产环境 */
-  prdAppEnv: string;
+    /*! 钉钉群机器人 */
+    dingTalk?: { url: string; asign: string }
+    /*! 项目配置 */
+    apps: Apps
+    /*! 项目环境配置 */
+    envs: Envs[]
+    /*! 项目生产环境 */
+    prdAppEnv: string
 }
 
 interface Envs {
-  /*! 环境名称 */
-  name?: string;
-  /*! tag后缀 */
-  identifier?: string;
-  /*! 环境所在的分支代码 */
-  releaseBranch?: string;
+    /*! 环境名称 */
+    name?: string
+    /*! tag后缀 */
+    identifier?: string
+    /*! 环境所在的分支代码 */
+    releaseBranch?: string
 }
 
 interface Apps {
-  /*! 标签 */
-  label: string;
-  /*! 项目名字 */
-  name: string;
-  /*! 项目路径 */
-  projectPath: string;
-  /*! 项目版本 */
-  version?: string;
+    /*! 标签 */
+    label: string
+    /*! 项目名字 */
+    name: string
+    /*! 项目路径 */
+    projectPath: string
+    /*! 项目版本 */
+    version?: string
 }
 
 export async function preBuild(configs: configOptions) {
-  const git = simplegit();
-  const diff = await git.diff();
-  if (diff)
-    return console.log(logSymbols.error, chalk.red('当前有未提交的修改'));
-  const {
-    apps,
-    dingTalk,
-    envs = [
-      { name: 'dev', identifier: 'dev' },
-      { name: 'sit', identifier: 'rc' },
-      { name: 'deploy', identifier: '' },
-    ],
-    prdAppEnv = 'deploy',
-  } = configs;
-  const envNames = envs.map((v) => v.name);
-  if (!apps.projectPath) return;
-  const packageJsonPath = path.join(apps.projectPath, './package.json');
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const packageJson = require(packageJsonPath);
-  const curVersion = packageJson.version;
+    const git = simplegit()
+    const diff = await git.diff()
+    if (diff)
+        return console.log(logSymbols.error, chalk.red('当前有未提交的修改'))
+    const {
+        apps,
+        dingTalk,
+        envs = [
+            { name: 'dev', identifier: 'dev' },
+            { name: 'sit', identifier: 'rc' },
+            { name: 'deploy', identifier: '' },
+        ],
+        prdAppEnv = 'deploy',
+    } = configs
+    const envNames = envs.map((v) => v.name)
+    if (!apps.projectPath) return
+    const packageJsonPath = path.join(apps.projectPath, './package.json')
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const packageJson = require(packageJsonPath)
+    const curVersion = packageJson.version
 
-  // 获取 env 参数
-  const args = yargs
-    // @ts-ignore
-    .usage('$0 <appEnv>', '构建前准备', (y) => {
-      y.positional('appEnv', {
-        describe: 'App Env',
-        choices: envNames,
-      }).option('remote', {
-        description: '远端仓库的名称，默认是 origin',
-        default: 'origin',
-      });
-    })
-    .version(false)
-    .help().argv;
-  const appEnv = args.appEnv;
-  // 环境配置
-  const envConfig: Envs = envs.find((v) => v.name === appEnv) || {};
-  // 版本后缀名，比如 dev 是 dev, sit 是 rc, deploy 是空的
-  const versionIdentifier = envConfig.identifier || '';
-  // 检查分支是否在对应环境的发布分支
-  const releaseBranch = envConfig.releaseBranch;
-  if (releaseBranch) {
-    const curBranch = await branch.getCurrentBranch();
-    if (curBranch !== releaseBranch) {
-      console.log(
-        chalk.bgRed(
-          `当前分支和当前 appEnv:${appEnv} 的发布分支不匹配!(${curBranch}!==${releaseBranch})`,
-        ),
-      );
-      return;
-    }
-    // 应用版本
-    try {
-      const selectVersion = await enquirer.prompt({
-        name: apps.name,
-        message: `请输入${apps.label}要打包的版本[当前：${packageJson.version}]`,
-        type: 'select',
-        choices: function () {
-          if (appEnv === prdAppEnv) {
-            return [
-              {
-                message: 'patch(小版本)',
-                name: 'patch',
-              },
-              {
-                message: 'minor(中版本)',
-                name: 'minor',
-              },
-              {
-                message: 'major(大版本)',
-                name: 'major',
-              },
-            ];
-          } else {
-            return [
-              {
-                message: 'prerelease(build 加一)',
-                name: 'prerelease',
-              },
-              {
-                message: 'prepatch(小版本)',
-                name: 'prepatch',
-              },
-              {
-                message: 'preminor(中版本)',
-                name: 'preminor',
-              },
-              {
-                message: 'premajor(大版本)',
-                name: 'premajor',
-              },
-            ];
-          }
-        },
-        format: function (value) {
-          return nextVersion(curVersion, value, versionIdentifier);
-        },
-        initial: appEnv === prdAppEnv ? 'patch' : 'prerelease',
-      });
-      if (!selectVersion) return console.log(chalk.red('取消打包'));
-      apps.version = await nextVersion(
-        curVersion,
-        selectVersion[apps.name],
-        versionIdentifier,
-      );
-    } catch (err) {
-      console.log(err);
-    }
-    if (!apps.version) {
-      return;
-    }
-    // 确认版本
-    const answers = await enquirer.prompt([
-      {
-        name: 'confirm',
-        message: `是否确认打包 ${apps.name}: ${apps.version}`,
-        type: 'confirm',
-      },
-    ]);
-    if (!answers.confirm) return console.log(chalk.red('取消打包'));
-    if (!semver.valid(apps.version))
-      return console.log(logSymbols.error, chalk.red('版本号格式错误'));
-    // 修改版本号
-    await changeVersion(apps.version, packageJson, packageJsonPath);
-    try {
-      //package.json 版本号
-      await git.add(apps.projectPath + '/*');
-      await git.commit(`prebuild: ${apps.version}`);
-      console.log(logSymbols.success, chalk.green('推送代码到远程中'));
-      await git.push('origin', releaseBranch);
-      console.log(logSymbols.success, chalk.green('推送代码成功'));
-      // const isExist = await git.show(`v${nextVersion}`);
-      await git.tag([`${apps.version}`]);
-      // if (!isExist) await git.tag([`v${nextVersion}`]);
-      await git.push(['origin', `${apps.version}`]);
-      console.log(logSymbols.success, chalk.green('推送tag成功'));
-      if (dingTalk) {
-        const url = await handleUrlAsign(dingTalk.url, dingTalk.asign);
-        const msg = `
+    // 获取 env 参数
+    const args = yargs
+        // @ts-ignore
+        .usage('$0 <appEnv>', '构建前准备', (y) => {
+            y.positional('appEnv', {
+                describe: 'App Env',
+                choices: envNames,
+            }).option('remote', {
+                description: '远端仓库的名称，默认是 origin',
+                default: 'origin',
+            })
+        })
+        .version(false)
+        .help().argv
+    const appEnv = args.appEnv
+    // 环境配置
+    const envConfig: Envs = envs.find((v) => v.name === appEnv) || {}
+    // 版本后缀名，比如 dev 是 dev, sit 是 rc, deploy 是空的
+    const versionIdentifier = envConfig.identifier || ''
+    // 检查分支是否在对应环境的发布分支
+    const releaseBranch = envConfig.releaseBranch
+    if (releaseBranch) {
+        const curBranch = await branch.getCurrentBranch()
+        if (curBranch !== releaseBranch) {
+            console.log(
+                chalk.bgRed(
+                    `当前分支和当前 appEnv:${appEnv} 的发布分支不匹配!(${curBranch}!==${releaseBranch})`
+                )
+            )
+            return
+        }
+        // 应用版本
+        try {
+            const selectVersion = await enquirer.prompt({
+                name: apps.name,
+                message: `请输入${apps.label}要打包的版本[当前：${packageJson.version}]`,
+                type: 'select',
+                choices: function () {
+                    if (appEnv === prdAppEnv) {
+                        return [
+                            {
+                                message: 'patch(小版本)',
+                                name: 'patch',
+                            },
+                            {
+                                message: 'minor(中版本)',
+                                name: 'minor',
+                            },
+                            {
+                                message: 'major(大版本)',
+                                name: 'major',
+                            },
+                        ]
+                    } else {
+                        return [
+                            {
+                                message: 'prerelease(build 加一)',
+                                name: 'prerelease',
+                            },
+                            {
+                                message: 'prepatch(小版本)',
+                                name: 'prepatch',
+                            },
+                            {
+                                message: 'preminor(中版本)',
+                                name: 'preminor',
+                            },
+                            {
+                                message: 'premajor(大版本)',
+                                name: 'premajor',
+                            },
+                        ]
+                    }
+                },
+                format: function (value) {
+                    return nextVersion(curVersion, value, versionIdentifier)
+                },
+                initial: appEnv === prdAppEnv ? 'patch' : 'prerelease',
+            })
+            if (!selectVersion) return console.log(chalk.red('取消打包'))
+            apps.version = await nextVersion(
+                curVersion,
+                selectVersion[apps.name],
+                versionIdentifier
+            )
+        } catch (err) {
+            console.log(err)
+        }
+        if (!apps.version) {
+            return
+        }
+        // 确认版本
+        const answers = await enquirer.prompt([
+            {
+                name: 'confirm',
+                message: `是否确认打包 ${apps.name}: ${apps.version}`,
+                type: 'confirm',
+            },
+        ])
+        if (!answers.confirm) return console.log(chalk.red('取消打包'))
+        if (!semver.valid(apps.version))
+            return console.log(logSymbols.error, chalk.red('版本号格式错误'))
+        // 修改版本号
+        await changeVersion(apps.version, packageJson, packageJsonPath)
+        try {
+            //package.json 版本号
+            await git.add(apps.projectPath + '/*')
+            await git.commit(`prebuild: ${apps.version}`)
+            console.log(logSymbols.success, chalk.green('推送代码到远程中'))
+            await git.push('origin', releaseBranch)
+            console.log(logSymbols.success, chalk.green('推送代码成功'))
+            // const isExist = await git.show(`v${nextVersion}`);
+            await git.tag([`${apps.version}`])
+            // if (!isExist) await git.tag([`v${nextVersion}`]);
+            await git.push(['origin', `${apps.version}`])
+            console.log(logSymbols.success, chalk.green('推送tag成功'))
+            if (dingTalk) {
+                const url = await handleUrlAsign(dingTalk.url, dingTalk.asign)
+                const msg = `
 ## 🎉🎉 [${apps.name}] 打包成功 🥳 version: **${apps.version}**
 - 操作人: ${process.env.GITLAB_USER_NAME || process.env.USER}
-;`;
-        notify(url, msg, apps.name);
-      }
-    } catch (err) {
-      if (dingTalk) {
-        const url = await handleUrlAsign(dingTalk.url, dingTalk.asign);
-        const msg = `
+;`
+                notify(url, msg, apps.name)
+            }
+        } catch (err) {
+            if (dingTalk) {
+                const url = await handleUrlAsign(dingTalk.url, dingTalk.asign)
+                const msg = `
 ## 🎉🎉 [${apps.name}] 打包失败 😭😭 version: **${apps.version}**
 - 操作人: ${process.env.GITLAB_USER_NAME || process.env.USER}
--原因: git提交失败
-;`;
-        notify(url, msg, apps.name);
-      }
-      console.log(`推送远程失败: + ${err}`);
-    }
+-原因: git提交失败: ${err}
+;`
+                notify(url, msg, apps.name)
+            }
+            console.log(`推送远程失败: + ${err}`)
+        }
 
-    return;
-  }
+        return
+    }
 }
